39bfeb0dd8a0aa3405efca1ee29044ed49d7f0e4
Shafqat Abbas <chshafqat4422@gmail.com>
2026-03-18 23:51:38 +0500

    Learn about angular routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+     path:"",
+     pathMatch:'full',
+     loadComponent:()=>{
+        return import('./home/home').then((m)=>m.Home)
+     }
+},
+{
+
+path:"todos",
+loadComponent:()=>{
+    return import('./todos/todos').then((m)=>m.Todos)
+}
+}
+   
+];
---
+++ src/app/components/todo-items/todo-items.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-items',
+  imports: [],
+  templateUrl: './todo-items.html',
+  styleUrl: './todo-items.scss',
+})
+export class TodoItems {}
